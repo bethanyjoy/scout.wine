d25c9d476f1fc2f182afeebe19f279700e2ed357
Bethany Nuechterlein <bethanyjoyn@gmail.com>
2023-01-26 22:17:57 -0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -233,7 +233,7 @@ for x in sl_orange_urls:
         title_text = product.find("div", class_="title").text.strip()
         price = product.find("div", class_="product-price").text.strip()
         link = product.find("a")['href']
-        image = product.get('srcset').split()[0]
+        # image = product.find("img")['srcset']
         store = 'silverlake'
         store_text = 'Silver Lake Wine'
         type = 'orange'
@@ -243,7 +243,7 @@ for x in sl_orange_urls:
             'Title_text': title_text,
             'Price': price,
             'Link': link,
-            'Image': image,
+            # 'Image': image,
             'Type': type,
             'Type_text': type_text,
             'Store': store,
